6901e0645e8777f836f840574cd511f8170ac767
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-06-04 22:14:41 +0200

    Implemented GreyscalePlugin

--- SepiaPlugin/SepiaPlugin.cs
+++ SepiaPlugin/SepiaPlugin.cs
@@ -4,10 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace SepiaPlugin
 {
     public class SepiaPlugin: IPlugin
     {
+        public TransformedBitmap doOperation(BitmapSource image)
+        {
+            TransformedBitmap tmp = new TransformedBitmap();
+            tmp.BeginInit();
+            tmp.Source = new FormatConvertedBitmap(image, PixelFormats.Gray8, BitmapPalettes.Gray256, 0.0);
+            tmp.EndInit();
+            return tmp;
+        }
+
+        public Button getPluginButton()
+        {
+            Button result = new Button();
+            Label label = new Label();
+            label.Content = "Sepia";
+            result.Content = label;
+
+            return result;
+        }
     }
 }
