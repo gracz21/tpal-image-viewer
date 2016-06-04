c97e76584b517bcd6a7ffbdaa2db0ba39f3f1d77
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-06-04 15:25:44 +0200

    Project init

---
+++ ResizePlugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResizePlugin
+{
+    public class Class1
+    {
+    }
+}
