df09546c3cc9255a2500281a6d680d6504af4bde
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-06-04 23:20:12 +0200

    Added RotateLeftPlugin class

--- ResizePlugin/RotatePlugin.cs
+++ ResizePlugin/RotatePlugin.cs
@@ -14,7 +14,25 @@ using System.Windows.Media;
 
 namespace RotatePlugin
 {
-    public class RotatePlugin: IPlugin
+    public class RotateLeftPlugin : IPlugin
+    {
+        public BitmapSource doOperation(BitmapSource image)
+        {
+            return new TransformedBitmap(image, new RotateTransform(-90));
+        }
+
+        public Button getPluginButton()
+        {
+            Button result = new Button();
+            Label label = new Label();
+            label.Content = "Rotate left";
+            result.Content = label;
+
+            return result;
+        }
+    }
+
+    public class RotateRightPlugin: IPlugin
     {
         public BitmapSource doOperation(BitmapSource image)
         {
@@ -25,7 +43,7 @@ namespace RotatePlugin
         {
             Button result = new Button();
             Label label = new Label();
-            label.Content = "Rotate";
+            label.Content = "Rotate right";
             result.Content = label;
 
             return result;
